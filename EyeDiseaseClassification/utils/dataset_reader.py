0e89cbb26acecd0ed0e2324722a228ce41cb0135
André Davis <CubicleJockey@users.noreply.github.com>
2024-01-08 17:46:53 -0800

    Re-factored to the known obsolete version of image reading.

--- EyeDiseaseClassification/utils/dataset_reader.py
+++ EyeDiseaseClassification/utils/dataset_reader.py
@@ -1,58 +1,20 @@
 import os
-from typing import List
 
 
-class EyeImage:
-    def __init__(self, image_path: str, image_label: str):
-        self.image_path = image_path
-        self.image_label = image_label
-
-    @property
-    def path(self):
-        return self.image_path
-
-    @property
-    def label(self):
-        return self.image_label
-
-
-def load_eye_dataset(images_folder: str) -> List[EyeImage]:
+def count_files_in_directory(directory: str) -> int:
     """
-    :param images_folder: string of the parent folder containing the images
-    :type images_folder: str
+    Counts all the files in a directory and its subdirectories.
 
-    :return: A dictionary containing the image path and its corresponding label.
+    :param directory: The path to the directory.
+    :type directory: str
+
+    :return: The total number of files in the directory and its subdirectories.
     """
-    eye_dataset: List[EyeImage] = list()
 
-    folders = os.listdir(images_folder)
-    for folder in folders:
-        current_path = os.path.join(images_folder, folder)
-        image_files = os.listdir(current_path)
-        for file in image_files:
-            image_path = os.path.join(current_path, file)
-            if image_path.lower().endswith(('.jpg', '.jpeg', '.png')):
-                eye_image = EyeImage(image_path, folder)
-                eye_dataset.append(eye_image)
-            else:
-                continue
+    total_files = 0
 
-    return eye_dataset
+    # Walk through all directories and files in the given directory
+    for root, dirs, files in os.walk(directory):
+        total_files += len(files)
 
-
-def eye_dataset_to_dictionary(dataset: List[EyeImage]) -> dict:
-    """
-    :param dataset: A list of EyeImage instances
-    :type dataset: List[EyeImage]
-
-    :return: Dictionary version of the list of EyeImages
-    """
-    eye_dataset_as_dict = {
-        'image-path': list(),
-        'image-label': list()
-    }
-    for eye_image in dataset:
-        eye_dataset_as_dict['image-path'].append(eye_image.path)
-        eye_dataset_as_dict['image-label'].append(eye_image.label)
-
-    return eye_dataset_as_dict
+    return total_files
---
+++ EyeDiseaseClassification/utils/kera_utils.py
@@ -0,0 +1,113 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+def create_image_datasets(directory: str,
+                          batch_size: int,
+                          image_size: tuple = (256, 256),
+                          validation_split: float = 0.2,
+                          test_split: float = 0.1) -> (tf.data.Dataset, tf.data.Dataset, tf.data.Dataset):
+    """
+    Creates image datasets for training, validation, and testing from a directory structure.
+
+    :param directory: Path to the main dataset directory.
+    :type directory: str
+
+    :param batch_size: Size of the batch.
+    :type batch_size: int
+
+    :param image_size: Size of the images (width, height).
+    :type image_size: tuple
+
+    :param validation_split: Float between 0 and 1, fraction of data to reserve for validation.
+    :type validation_split: float
+
+    :param test_split: Float between 0 and 1, fraction of validation data to reserve for testing.
+    :type test_split: float
+
+    :return: A tuple of three `tf.data.Dataset` objects for training, validation, and testing.
+    """
+
+    # Creating the training and initial validation dataset
+    train_dataset = tf.keras.utils.image_dataset_from_directory(
+        directory,
+        labels='inferred',
+        label_mode='categorical',
+        class_names=None,
+        color_mode='rgb',
+        batch_size=batch_size,
+        image_size=image_size,
+        shuffle=True,
+        seed=123,
+        validation_split=validation_split,
+        subset='training',
+        interpolation='bilinear',
+        follow_links=False,
+        crop_to_aspect_ratio=False
+    )
+
+    validation_dataset = tf.keras.utils.image_dataset_from_directory(
+        directory,
+        labels='inferred',
+        label_mode='categorical',
+        class_names=None,
+        color_mode='rgb',
+        batch_size=batch_size,
+        image_size=image_size,
+        shuffle=True,
+        seed=123,
+        validation_split=validation_split,
+        subset='validation',
+        interpolation='bilinear',
+        follow_links=False,
+        crop_to_aspect_ratio=False
+    )
+
+    # Calculating the number of batches to take for test dataset from the validation dataset
+    total_val_batches = validation_dataset.cardinality().numpy()
+    test_batches = int(total_val_batches * test_split)
+
+    # Splitting the validation dataset into validation and test datasets
+    test_dataset = validation_dataset.take(test_batches)
+    validation_dataset = validation_dataset.skip(test_batches)
+
+    return train_dataset, validation_dataset, test_dataset
+
+
+def display_batch_of_images(dataset: tf.data.Dataset
+                            , class_names: list[str]
+                            , rows: int = 5
+                            , cols: int = 5
+                            , fig_size: tuple[float, float] = (10, 10)) -> None:
+    """
+    Displays a batch of images in a grid format.
+
+    :param dataset: A TensorFlow Dataset object containing image batches.
+    :type dataset: tf.data.Dataset
+
+    :param rows: Number of rows in the grid.
+    :type rows: int
+
+    :param cols: Number of columns in the grid.
+    :type cols: int
+
+    :param fig_size: Size of the figure to plotted
+    :type fig_size: tuple[float, float]
+
+    :return: None
+    """
+
+    # Take one batch from the dataset
+    for images, labels in dataset.take(1):
+        # Initialize the plot
+        plt.figure(figsize=fig_size)
+
+        # Total number of images to display (rows * cols)
+        total_images = rows * cols
+
+        for i in range(total_images):
+            plt.subplot(rows, cols, i + 1)
+            plt.imshow(images[i].numpy().astype("uint8"))
+            label_index = labels[i].numpy().argmax()
+            plt.title(class_names[label_index])
+            plt.axis("off")
